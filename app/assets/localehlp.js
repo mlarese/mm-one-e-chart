9f5db6b796bbf6b28cea63088cd7d1c748a74c14
mlarese <mauro.larese@gmail.com>
2017-11-03 04:00:14 +0100

    update

--- app/assets/localehlp.js
+++ app/assets/localehlp.js
@@ -1,11 +1,15 @@
 import numeral from 'numeral'
+import 'numeral/locales/en-gb'
+import 'numeral/locales/it'
+import 'numeral/locales/fr'
+import 'numeral/locales/es'
+import 'numeral/locales/de'
 
 export const changeLocale = (locale) => {
   if (locale === 'en') {
     locale = 'en-gb'
   }
-  require('numeral/locales/'+locale)
+  // require('numeral/locales/'+locale)
   numeral.locale(locale)
-
   return numeral.localeData()
 }
