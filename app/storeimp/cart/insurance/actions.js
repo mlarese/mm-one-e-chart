839b012e0ad3193d4838a99817163d8ad464c1df
maurolarese <asemota91>
2017-11-04 07:15:16 +0100

    update

--- app/storeimp/cart/insurance/actions.js
+++ app/storeimp/cart/insurance/actions.js
@@ -9,5 +9,24 @@ export const actions = {
   removeInsurance ({commit, dispatch}) {
     commit('setInsuranceType', null)
     commit('hasInsurance', false)
+  },
+  quoteInsurance ({commit, dispatch, getters, rootGetters}, {total, checkIn, checkOut, adults, children, insuranceType}) {
+    const url = '/booking/allianz/quote'
+    const userLanguageCode = rootGetters['app/userLanguageCode']
+
+    const options = {
+      headers: {
+        StructureId: getters.structureId,
+        PortalId: getters.portalId,
+        UserLanguageCode: userLanguageCode,
+        Total: total,
+        InsuranceType: insuranceType,
+        Adults: adults,
+        Checkin: checkIn,
+        Checkout: checkOut,
+        Children: children
+      }
+    }
+    return dispatch('api/get', {url, options}, {root: true})
   }
 }
