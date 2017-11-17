c18f133b5877f71f27d6548d60139e69da16adf8
maurolarese <asemota91>
2017-11-17 07:05:14 +0100

    update

---
+++ appoi/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1> app from App component {{title}}</h1>
+    </div>
+</template>
+<script>
+  import {mapState} from 'vuex'
+  export default {
+    computed: {
+      ...mapState(['title'])
+    }
+  }
+</script>
